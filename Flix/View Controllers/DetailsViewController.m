ae7f045e77b3e7d6cb04171236b214001cae755a
Abenezer Molla <abenezer@minerva.kgi.edu>
2021-06-24 13:36:18 -0700

    PrepareForSegue

---
+++ Flix/View Controllers/DetailsViewController.m
@@ -0,0 +1,35 @@
+//
+//  DetailsViewController.m
+//  Flix
+//
+//  Created by abenezermolla on 6/24/21.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *backDropView;
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

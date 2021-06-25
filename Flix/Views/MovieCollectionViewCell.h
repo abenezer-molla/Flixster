56075e43da5099330ed7424806d9c0b4e4ef98e9
Abenezer Molla <abenezer@minerva.kgi.edu>
2021-06-25 00:45:40 -0700

    Required Checkpoint.

---
+++ Flix/Views/MovieCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionViewCell.h
+//  Flix
+//
+//  Created by abenezermolla on 6/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flix/Views/MovieCollectionViewCell.m
@@ -0,0 +1,12 @@
+//
+//  MovieCollectionViewCell.m
+//  Flix
+//
+//  Created by abenezermolla on 6/24/21.
+//
+
+#import "MovieCollectionViewCell.h"
+
+@implementation MovieCollectionViewCell
+
+@end
